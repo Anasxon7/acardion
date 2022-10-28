aaeb00bc0e08c6d27fe1b4a85c1523e4dd372657
Madaminov <muhammadanasxon6694@gmail.com>
2022-10-28 22:38:18 +0500

    add

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
-function handleClick(evt) {
-    const content = evt.target.nextElementSibling
-    if (!evt.target.parentNode.classList.contains('open')) {
-        content.style.height = `${content.scrollHeight}px`
-        evt.target.parentNode.classList.add('open')
-    } else {
-        content.style.height = '0px'
-        evt.target.parentNode.classList.remove('open')
-    }
-}
+const accordionContent = document.querySelectorAll('.accordion-content');
+
+accordionContent.forEach( (item, index) => {
+    console.log(item);
+})
