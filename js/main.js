97bfd53f4fe46b501f0c60119ffbdc0eedde9e03
Madaminov <muhammadanasxon6694@gmail.com>
2022-10-28 22:47:10 +0500

    add

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 const accordionContent = document.querySelectorAll('.accordion-content');
 
 accordionContent.forEach( (item, index) => {
-    console.log(item);
+    let header = item.querySelector('header');
+    header.addEventListener('click', () =>  {
+        item.classList.toggle('open')
+
+        let description = item.querySelector('.description')
+        if(item.classList.contains("open")) {
+            description.style.height = `${description.scrollHeight}px`
+        } else {
+            description.style.height = '0px';
+        }
+        console.log(description);
+
+    })
 })
